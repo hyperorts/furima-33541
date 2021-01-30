c61f83d58c82b41dca9fed6361f64267a762119c
hyperorts <loss.dealer@gmail.com>
2021-01-30 20:42:57 +0900

    新規登録画面の問題点修正
    新規登録画面
    ・パスワードは、半角英数字混合
    ユーザー本名のフリガナは全角での入力が必須
    これらを解決した。

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/}
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/}
+  validates :family_name_kana, format: { with:/\A[ァ-ヶー－]+\z/}
+  validates :first_name_kana, format: { with:/\A[ァ-ヶー－]+\z/}
+  validates :password, format: { with:  /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
   validates :birthday, presence: true
-  validates :password, presence: true
 
   has_many :items
   has_many :deal
-
-
 end
