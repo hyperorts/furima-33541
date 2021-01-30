a7cb897d72a40452731f3b4caa27374124a46276
hyperorts <loss.dealer@gmail.com>
2021-01-30 12:35:39 +0900

    登録は出来るが制限がない状態
    新規登録は可能だがパスワードの英+数指定が機能していない。
    ログインはsession newを編集して可能となった。
    また編集画面等は未実装

--- db/migrate/20210129175011_devise_create_users.rb
+++ db/migrate/20210129175011_devise_create_users.rb
@@ -4,9 +4,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,           null: false
       t.string :family_name,        null: false
       t.string :first_name,         null: false
       t.string :family_name_kana,   null: false
@@ -19,7 +19,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
 
       ## Rememberable
       t.datetime :remember_created_at
-
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
