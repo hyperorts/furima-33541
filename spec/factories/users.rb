00d9f95901cd2579821c08000f87348046721c99
hyperorts <loss.dealer@gmail.com>
2021-02-01 12:33:37 +0900

    テストコード提出準備完了

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname {Faker::Name.male_first_name}
     email {Faker::Internet.free_email}
-    password {"1aA" + Faker::Internet.password(min_length: 6)}
+    password {"1a" + Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     family_name {"阿阿阿"}
     first_name {"阿阿阿"}
