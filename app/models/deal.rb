8f2a3beb4a28e2291dc4568e40802442b7334dc8
hyperorts <loss.dealer@gmail.com>
2021-02-08 03:53:32 +0900

    購入機能レビュー依頼
    クレジットカードの情報は購入の都度入力させること
    ログイン状態のユーザーが、URLを直接入力して売却済み商品の商品購入ページへ遷移しようとすると、トップページに遷移すること
    エラーハンドリングができていること（適切では無い値が入力された場合、情報は保存されず、エラーメッセージを出力させること）
    ログアウト状態のユーザーは、URLを直接入力して商品購入ページに遷移しようとすると、商品の販売状況に関わらずログインページに遷移すること
    配送先の情報として、郵便番号・都道府県・市区町村・番地・電話番号が必須であること
    電話番号にはハイフンは不要で、11桁以内であること（09012345678となる）
    必要な情報を適切に入力すると、商品の購入ができること
    クレジットカード情報は必須であり、正しいクレジットカードの情報で無いときは決済できないこと
    クレジットカード決済ができること
    入力に問題がある状態で購入ボタンが押されたら、購入ページに戻りエラーメッセージが表示されること
    ログイン状態の出品者が、URLを直接入力して自身の出品した商品購入ページに遷移しようとすると、トップページに遷移すること
    郵便番号にはハイフンが必要であること（123-4567となる）
    配送先の住所情報も購入の都度入力させること
    商品購入ページでは、一覧や詳細ページで選択した商品の情報が出力されること
    購入が完了したら、トップページまたは購入完了ページに遷移すること

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,5 +1,5 @@
 class Deal < ApplicationRecord
   belongs_to :user
   belongs_to :item
-  has_one :addresse
+  has_one :address
 end
---
+++ spec/models/buyer_spec.rb
@@ -0,0 +1,89 @@
+require 'rails_helper'
+
+RSpec.describe Buyer, type: :model do
+  before do
+    seller = FactoryBot.create(:user)
+    buyer = FactoryBot.create(:user)
+    item =  FactoryBot.create(:item, user_id:seller.id)
+    @buyer = FactoryBot.build(:buyer, user_id: buyer.id, item_id: item.id)
+    sleep(2)
+  end
+  describe '商品購入' do
+    context '内容に問題がある場合' do
+      it "郵便番号が空では購入不可" do
+        @buyer.postal_code = ""
+        @buyer.valid?
+        expect(@buyer.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it "郵便番号に-（ハイフン）が必須" do
+        @buyer.postal_code = "0000000"
+        @buyer.valid?
+        expect(@buyer.errors.full_messages).to include("Postal code is invalid")
+      end
+      it "都道府県が空では購入不可" do
+        @buyer.prefecture_id = ""
+        @buyer.valid?
+        expect(@buyer.errors.full_messages).to include("Prefecture is not a number")
+      end
+      it "都道府県が--では購入不可" do
+        @buyer.prefecture_id = 1
+        @buyer.valid?
+        expect(@buyer.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+      it "市区町村が空では購入不可" do
+        @buyer.municipality  = ""
+        @buyer.valid?
+        expect(@buyer.errors.full_messages).to include("Municipality can't be blank")
+      end
+      it "市区町村は全角での入力が必須" do
+        @buyer.municipality = "a"
+        @buyer.valid?
+        expect(@buyer.errors.full_messages).to include("Municipality is invalid")
+      end
+      it "番地なしでは購入不可" do
+        @buyer.address_number = ""
+        @buyer.valid?
+        expect(@buyer.errors.full_messages).to include("Address number can't be blank")
+      end
+      it "電話番号なしでは購入不可" do
+        @buyer.phone_number  = ""
+        @buyer.valid?
+        expect(@buyer.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it "電話番号は半角数字での入力が必須" do
+        @buyer.phone_number  = "０９０１２３４５６７８"
+        @buyer.valid?
+        expect(@buyer.errors.full_messages).to include("Phone number is invalid")
+      end
+      it "電話番号は１２文字以上では購入不可" do
+        @buyer.phone_number  = "090123456789"
+        @buyer.valid?
+        expect(@buyer.errors.full_messages).to include( "Phone number is invalid")
+      end
+      it "user_idなしでは購入不可" do
+        @buyer.user_id = ""
+        @buyer.valid?
+        expect(@buyer.errors.full_messages).to include("User can't be blank")
+      end
+      it "item_idなしでは購入不可" do
+        @buyer.item_id = ""
+        @buyer.valid?
+        expect(@buyer.errors.full_messages).to include("Item can't be blank")
+      end
+      it "tokenなしでは購入不可" do
+        @buyer.token = ""
+        @buyer.valid?
+        expect(@buyer.errors.full_messages).to include("Token can't be blank")
+      end
+      context '内容に問題がない場合' do
+        it "上記の条件が全て満たされた場合は購入可" do
+          expect(@buyer).to be_valid
+          end
+        it "建物名なしでも購入可" do
+          @buyer.building_name = ""
+          expect(@buyer).to be_valid
+        end
+      end
+    end
+  end
+end
