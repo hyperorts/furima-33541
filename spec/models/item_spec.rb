7051093c59acde3b0b3ea9505ca4b251a10f006f
hyperorts <loss.dealer@gmail.com>
2021-02-08 10:58:50 +0900

    レビュー修正１回目

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -107,10 +107,10 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
-      context '内容に問題がない場合' do
-        it '商品出品ができる時' do
-          expect(@item).to be_valid
-        end
+    end
+    context '内容に問題がない場合' do
+      it '商品出品ができる時' do
+        expect(@item).to be_valid
       end
     end
   end
