8ac0fb5930c8dff9c381b3dfc8d7b1a92a5dbebe
hyperorts <loss.dealer@gmail.com>
2021-02-14 17:51:25 +0900

    エラー文を正しい文法へ変更
    messageオプションを利用した

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,7 +16,7 @@ class Item < ApplicationRecord
     validates :image
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
-  with_options presence: true, numericality: { other_than: 1 } do
+  with_options presence: true, numericality: { other_than: 1, message: 'を選んでください' } do
     validates :category_id
     validates :status_id
     validates :shipping_cost_id
--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,8 @@ module Furima33541
 
     # 日本語化の言語設定
     config.i18n.default_locale = :ja
-
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+    config.active_model.i18n_customize_full_message = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
