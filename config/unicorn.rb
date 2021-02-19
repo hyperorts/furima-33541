f72a25ba4924e1ed503027efdc25d9a223227ad5
hyperorts <loss.dealer@gmail.com>
2021-02-20 08:49:52 +0900

    デプロイ自動化
    capistrano使用

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,23 +1,23 @@
-#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../', __FILE__)
+#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておいた
+app_path = File.expand_path('../../../', __FILE__)  # 「../」が一つ増えた
 
-#アプリケーションサーバの性能を決定する
+#アプリケーションサーバの性能を決定した
 worker_processes 1
 
-#アプリケーションの設置されているディレクトリを指定
-working_directory app_path
+#アプリケーションの設置されているディレクトリを指定した
+working_directory "#{app_path}/current"  # 「current」を指定した
 
 #Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/tmp/pids/unicorn.pid"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"  # 「shared」の中を参照するよう変更した
 
 #ポート番号を指定
-listen "#{app_path}/tmp/sockets/unicorn.sock"
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"  # 「shared」の中を参照するよう変更した
 
 #エラーのログを記録するファイルを指定
-stderr_path "#{app_path}/log/unicorn.stderr.log"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"  # 「shared」の中を参照するよう変更した
 
 #通常のログを記録するファイルを指定
-stdout_path "#{app_path}/log/unicorn.stdout.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"  # 「shared」の中を参照するよう変更した
 
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
